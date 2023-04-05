172c107ab118161f008792e63313aeb492e2bf86
otter00 <ivlevanastia2001@mail.ru>
2023-04-05 19:12:18 +0300

    fake button + styles

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,8 @@
 /*      AUTHORIZATION CHANGE        */
 
-let button = document.querySelector(".login-form__btn");
-let form = document.querySelector('form');
+const button__true = document.querySelector(".login-form__btn");
+const button__false = document.querySelector('.login-form__btn-fake');
+const form = document.querySelector('form');
 let errors = [];
 
 const genderM = document.getElementById('genderM');
@@ -36,7 +37,7 @@ function checkValidity(input) {
         }
 }
 
-button.addEventListener('click', ()=>{
+button__true.addEventListener('click', ()=>{
 //Проверка для всех полей
 	//получаем все инпуты
     let inputs = document.querySelectorAll("input");
@@ -87,6 +88,60 @@ button.addEventListener('click', ()=>{
         location.reload(); //перезагружаем страницу, если всё введено корректно, 
         //чтобы очистить поля для дальнейшего ввода
         window.location.href = 'pages/guide.html';
+    }
+
+    errors = []; //чистим массив
+});
+
+button__false.addEventListener('click', ()=>{
+//Проверка для всех полей
+	//получаем все инпуты
+    let inputs = document.querySelectorAll("input");
+
+    regexp = /[A-Za-zА-Яа-яЁё]/gi; 
+    emailRegexp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
+
+	//перебираем их и на каждый вызываем функцию валидации
+    for (let input of inputs) {
+        //console.log(input);
+        checkValidity(input);
+    }
+
+    if (genderM.checked) {
+        gender1 = true; 
+        gender2 = false;
+        gender3 = false;
+    }
+    if (genderW.checked) {
+        gender2 = true; 
+        gender1 = false;
+        gender3 = false;
+    }
+    if (genderA.checked) {
+        gender3 = true;
+        gender2 = false; 
+        gender1 = false;
+    }
+    if ((gender1 == false) && (gender2 == false) && (gender3 == false)) {
+        errors.push(`Поле gender не заполнено!`);
+    }
+
+    if (!document.getElementById("email__input").value.match(emailRegexp)) {
+        errors.push(`${document.getElementById("email__input").name} некорректно`);
+    }
+
+    if (!document.getElementById("login__input").value.match(regexp)) {
+        errors.push(`${document.getElementById("login__input").name} некорректно`);
+    }
+
+	//выводим ошибки в div 
+    let errorDiv = document.querySelector('.errors__info');
+    errorDiv.innerHTML = errors.join('\n');
+
+    if (errors.length == 0) {
+        alert(`Ой, а кнопка-то фальшивая ))))0))00)`);
+        location.reload(); //перезагружаем страницу, если всё введено корректно, 
+        //чтобы очистить поля для дальнейшего ввода
     }
 
     errors = []; //чистим массив
