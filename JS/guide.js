0ab2a31fa674cbf57f03683040615947cf804ce4
otter00 <ivlevanastia2001@mail.ru>
2023-04-08 22:09:22 +0300

    some JS

---
+++ JS/guide.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    setTimeout(function run() {
+        alert('Здравствуйте! Наши двери всегда для вас закрыты - выходите!');
+    }, 1000);
+});
