9842f27ad7dfeaf673436ce8f5c0918b422ba4bc
otter00 <ivlevanastia2001@mail.ru>
2023-04-08 22:11:13 +0300

    set timeout

--- JS/guide.js
+++ JS/guide.js
@@ -1,5 +1,3 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    setTimeout(function run() {
-        alert('Здравствуйте! Наши двери всегда для вас закрыты - выходите!');
-    }, 1000);
+    setTimeout(() => alert('Здравствуйте! Наши двери всегда для вас закрыты - выходите!'), 1000);
 });
