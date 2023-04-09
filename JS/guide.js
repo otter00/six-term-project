621727a7828f580ce1f536bd533f7c8f99c0d290
otter00 <ivlevanastia2001@mail.ru>
2023-04-09 22:42:36 +0300

    hiding functionality added

--- JS/guide.js
+++ JS/guide.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     setTimeout(() => alert('Здравствуйте! Наши двери всегда для вас открыты - выходите!'), 1000);
 });
+
+let heroBlock = document.querySelector('.hero-block__container');
+
+document.querySelector('.excursions').addEventListener('click', ()=>{
+    heroBlock.classList.toggle("hidden");
+})
