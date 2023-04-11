5aa92abab340a61d25ca2f16759812b53fd58256
otter00 <ivlevanastia2001@mail.ru>
2023-04-11 21:39:03 +0300

    styles + cards

--- JS/guide.js
+++ JS/guide.js
@@ -1,9 +1,14 @@
+//      НЕ ЗАБЫТЬ РАСКОММЕНТИРОВАТЬ         
+
 document.addEventListener('DOMContentLoaded', ()=> {
-    setTimeout(() => alert('Здравствуйте! Наши двери всегда для вас открыты - выходите!'), 1000);
+    //setTimeout(() => alert('Здравствуйте! Наши двери всегда для вас открыты - выходите!'), 1000);
 });
 
 let heroBlock = document.querySelector('.hero-block__container');
+let excursionsBlock = document.querySelector('.excursions__container');
 
+// Скрываем-открываем блок в зависимости от пришедшего запроса
 document.querySelector('.excursions').addEventListener('click', ()=>{
     heroBlock.classList.toggle("hidden");
+    excursionsBlock.classList.toggle("hidden");
 })
