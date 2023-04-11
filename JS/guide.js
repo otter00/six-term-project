66cc2368c49e622253b80f44d7393504d29bd61a
otter00 <ivlevanastia2001@mail.ru>
2023-04-11 22:23:50 +0300

    gallery

--- JS/guide.js
+++ JS/guide.js
@@ -7,22 +7,33 @@ document.addEventListener('DOMContentLoaded', ()=> {
 let heroBlock = document.querySelector('.hero-block__container');
 let excursionsBlock = document.querySelector('.excursions__container');
 let attractionsBlock = document.querySelector('.attractions__container');
+let galleryBlock = document.querySelector('.gallery__container');
 
 // Скрываем-открываем блок в зависимости от пришедшего запроса
 document.querySelector('.excursions').addEventListener('click', ()=>{
     heroBlock.classList.add("hidden");
     excursionsBlock.classList.remove("hidden");
     attractionsBlock.classList.add("hidden");
+    galleryBlock.classList.add("hidden");
 })
 
 document.querySelector('.attr').addEventListener('click', ()=>{
     heroBlock.classList.add("hidden");
     excursionsBlock.classList.add("hidden");
     attractionsBlock.classList.remove("hidden");
+    galleryBlock.classList.add("hidden");
 })
 
 document.querySelector('.fa').addEventListener('click', ()=>{
     heroBlock.classList.remove("hidden");
     excursionsBlock.classList.add("hidden");
     attractionsBlock.classList.add("hidden");
+    galleryBlock.classList.add("hidden");
+})
+
+document.querySelector('.gallery').addEventListener('click', ()=>{
+    heroBlock.classList.add("hidden");
+    excursionsBlock.classList.add("hidden");
+    attractionsBlock.classList.add("hidden");
+    galleryBlock.classList.remove("hidden");
 })
