f91c42341fbbadb00111c4f12f42e8d59e10dd06
otter00 <ivlevanastia2001@mail.ru>
2023-04-13 20:46:14 +0300

    content show edited

--- JS/guide.js
+++ JS/guide.js
@@ -4,12 +4,44 @@ document.addEventListener('DOMContentLoaded', ()=> {
     //setTimeout(() => alert('Здравствуйте! Наши двери всегда для вас открыты - выходите!'), 1000);
 });
 
+let heroBlock = document.querySelector('.hero-block__container');
+let sections = document.querySelectorAll('section');
+let links = document.querySelectorAll('a');
+
+console.log(links);
+console.log(sections);
+console.log(heroBlock)
+
+
+// add hidden class for hero block if at least one link has been clicked
+// we have hero block shown by default as div JS
+// we have other blocks hidden by default using css targets links
+let checkTabs = (item) => {
+    console.log(item)
+    for (var i = 0, n = links.length; i < n; i++) {
+        //allLinks[i].addEventListener('click', function (event) {});
+        links[i].onclick = function () {
+            
+            // if(heroBlock.classList.contains('hidden'))  {
+            //     heroBlock.classList.remove("hidden");   
+            // }  
+        heroBlock.classList.add("hidden"); 
+    }
+
+    item.addEventListener('click', ()=>{
+        if(item.classList.contains('fa')) {
+            heroBlock.classList.remove('hidden');
+        }
+    })
+}
+}
+links.forEach(checkTabs);
+
 
 
 
 // Ниже вообще хрень какая-то... (переделано на css со ссылками)
 
-// let heroBlock = document.querySelector('.hero-block__container');
 // let excursionsBlock = document.querySelector('.excursions__container');
 // let attractionsBlock = document.querySelector('.attractions__container');
 // let galleryBlock = document.querySelector('.gallery__container');
