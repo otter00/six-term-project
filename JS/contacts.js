e5272019507262ff078f95a85e6c6bbadeee000d
otter00 <ivlevanastia2001@mail.ru>
2023-05-03 22:05:07 +0300

    form sending 404 cartoon page added

---
+++ JS/contacts.js
@@ -0,0 +1,5 @@
+let contact__btn = document.querySelector('.section__btn--dark');
+
+contact__btn.addEventListener('click', ()=>{
+    window.location.href = 'https://secretsgravityfalls.tilda.ws/en/oops';
+});
